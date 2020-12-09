56fe4de2fec915a5b2da12a737405b224849c85c
MichaelTrzaskoma <74200729+MichaelTrzaskoma@users.noreply.github.com>
2020-12-09 18:59:51 -0500

    Update Nutrifetch Backend.py
    Added comments

--- Backend/Nutrifetch Backend.py
+++ Backend/Nutrifetch Backend.py
@@ -4,7 +4,9 @@ from urllib.request import urlopen
 #upc = '044000032029'
 #upc = '044000000615'
 upc = '688267000263'
-#Dictionary for daily values, current numbers as place holders 
+
+
+#Dictionary for daily values, current numbers as place holders, not actual values
 dailyVal = {
     'fat' : 1,
     'saturated-fat' : 2,
@@ -18,6 +20,11 @@ dailyVal = {
     'potassium' : 10, 
 }
 
+
+
+#Checks if the upc exists or not
+#input : upc code
+#output : boolean
 def upcCheck(upc):
     url = "https://world.openfoodfacts.org/api/v0/produc/" + upc + ".json"
     with urlopen(url) as urla:
@@ -27,26 +34,58 @@ def upcCheck(upc):
         return False 
     if check['status'] == 1:
         return True
+    
 
+    
+#Takes the upc code and returns array of information 
+#Input UPC code
+#Output returns list with each index as:
+# 0 : product name (as a string)
+# 1 : ingredients (as a string)
+# 2 : allergens (as a string)
+# 3 : calories (as a string)
+#
+# Nutrient values as arrays below[Nutrient name, 
+#                                 nutrient value with unit(if product contains it, emptry string if not), 
+#                                 nutrient daily value percent(if nutrient exists and  has daily value, empty string if not)]
+# 4 : 'fat' 
+# 5 : 'staturated-fat' 
+# 6 : 'trans-fat'
+# 7 : 'cholesterol' 
+# 8 : 'sodium' 
+# 9 : 'carbohydrates' 
+# 10 : 'fiber' 
+# 11 : 'sugars'
+# 12 : 'protiens' 
+# 13 : 'vitamin-d' 
+# 14 : 'calcium' 
+# 15 : 'iron' 
+# 16 : 'potassium'
 def upcNutrition(upc):
+    #Loads the api json file
     url = "https://world.openfoodfacts.org/api/v0/produc/" + upc + ".json"
     with urlopen(url) as urla:
         s = urla.read()
     productFile = json.loads(s)
-
+    
+    #Array that contains the returned product information 
     productInformation = []
-
+    
+    #Dictionary ref for general information
     genProductDict = productFile['product']
 
-
+    #Name, ingredients, allergends
     productInformation.append(genProductDict['product_name'])
     productInformation.append(genProductDict['ingredients_text'])
     productInformation.append(genProductDict['allergens_from_ingredients'])
-
+    
+    #Dictionary ref for nutrition info of product
     nutriRef = genProductDict['nutriments']
-
+    
+    #Calories
     productInformation.append(nutriRef['energy-kcal'])
 
+    #Current nutrients list
     nutritionFields = ['fat', 
                         'staturated-fat', 
                         'trans-fat', 
@@ -61,12 +100,13 @@ def upcNutrition(upc):
                         'iron', 
                         'potassium',]
 
-
+    #Nutrients without daily values 
     noDaily = ['protiens', 
                 'sugars', 
                 'trans-fat']
     
-
+    #Loops through nutrient list, appends a list of nutrient : ['Name', 'Val', 'Percent']
+    #If nutrient doesn't exist, still will append Name with emptry string for Val and Percent
     for i in range(len(nutritionFields)):
         field = nutritionFields[i]
         fieldStats = []
@@ -92,4 +132,6 @@ def upcNutrition(upc):
 
     return productInformation
 
+############################################################
+#Test of upcNutrition, should return string of information 
 print(upcNutrition(upc))
